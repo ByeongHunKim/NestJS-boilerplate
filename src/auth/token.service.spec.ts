51f4ac9d8a15c6fc976a217631ff85558a01eca1
ByeongHunKim <rkwhr912@naver.com>
2023-08-30 16:42:01 +0900

    test : token service code

--- src/auth/token.service.spec.ts
+++ src/auth/token.service.spec.ts
@@ -1,13 +1,9 @@
 import { Test, TestingModule } from '@nestjs/testing'
-import {
-  AccessTokenPayload,
-  TokenService,
-  TokenType,
-} from '@/src/auth/token.service'
 import { JwtModule, JwtService } from '@nestjs/jwt'
 import { ConfigService } from '@nestjs/config'
 import { User } from '@prisma/client'
-import { testUser } from '@/test/fixtures/user.fixture'
+import { AccessTokenPayload, TokenService, TokenType } from './token.service'
+import { testUser } from '../../test/fixtures/user.fixture'
 
 describe('TokenService', () => {
   let jwtService: JwtService
@@ -49,5 +45,52 @@ describe('TokenService', () => {
 
   it('should be defined', () => {
     expect(tokenService).toBeDefined()
+  })
+
+  describe('#issueToken', () => {
+    it('issue token should return jwt token', async () => {
+      const refreshToken = await tokenService.issueToken(
+        TokenType.REFRESH,
+        user,
+      )
+      expect(refreshToken.length > 0).toBeTruthy()
+
+      const accessToken = await tokenService.issueToken(TokenType.ACCESS, user)
+      expect(accessToken.length > 0).toBeTruthy()
+    })
+
+    const expectedRefreshTokenPayloadKeys = ['iat', 'exp', 'iss', 'sub']
+    it(`refresh token should have only ${expectedRefreshTokenPayloadKeys.join(
+      ', ',
+    )} ${expectedRefreshTokenPayloadKeys.length} payload`, async () => {
+      const refreshToken = await tokenService.issueToken(
+        TokenType.REFRESH,
+        user,
+      )
+      const payload = jwtService.decode(refreshToken)
+      expect(Object.keys(payload!)).toHaveLength(
+        expectedRefreshTokenPayloadKeys.length,
+      )
+      expectedRefreshTokenPayloadKeys.forEach((payloadKey) => {
+        expect(payload![payloadKey]).toBeDefined()
+      })
+    })
+  })
+
+  describe('#verifyToken', () => {
+    it('verifyToken should return token payload when token string is correct', async () => {
+      const accessToken = await tokenService.issueToken(TokenType.ACCESS, user)
+      const payload = await tokenService.verifyToken<AccessTokenPayload>(
+        accessToken,
+      )
+      expect(payload).toBeDefined()
+    })
+
+    it('verifyToken should throw error when token string is incorrect', async () => {
+      const notIncorrectToken = 'not.incorrect'
+      await expect(
+        tokenService.verifyToken<AccessTokenPayload>(notIncorrectToken),
+      ).rejects.toThrowError()
+    })
   })
 })
