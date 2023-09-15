c24cf7ed073c3473c5cfb503d219ff2f1fc8c3ae
ByeongHunKim <rkwhr912@naver.com>
2023-09-16 03:17:55 +0900

    chore: Integrate HealthModule into AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,7 @@ import { ConfigModule } from '@nestjs/config'
 import { APP_GUARD } from '@nestjs/core'
 import { RolesGuard } from '@/src/auth/rbac/roles.guard'
 import { LoggerModule } from '@/src/logger.module'
+import { HealthModule } from '@/src/health/health.module'
 
 @Module({
   imports: [
@@ -13,8 +14,10 @@ import { LoggerModule } from '@/src/logger.module'
       load: [config],
       isGlobal: true,
     }),
+    HealthModule,
     LoggerModule,
     AuthModule,
+    HealthModule,
   ],
   providers: [
     {
