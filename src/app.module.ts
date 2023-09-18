1fb172f52b0bbb094413efb2b959e0b60d41b1d3
ByeongHunKim <rkwhr912@naver.com>
2023-09-18 23:17:08 +0900

    feature : handling multiple environment in AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common'
 import { AuthModule } from '@/src/auth/auth.module'
-import config from '@/config'
+import { loadConfig, loadEnvFilePath } from '@/config'
 import { ConfigModule } from '@nestjs/config'
 import { APP_GUARD } from '@nestjs/core'
 import { RolesGuard } from '@/src/auth/rbac/roles.guard'
@@ -10,8 +10,8 @@ import { HealthModule } from '@/src/health/health.module'
 @Module({
   imports: [
     ConfigModule.forRoot({
-      envFilePath: ['.env.local', '.env'], // todo using loadEnvFilePath() for handling many environment
-      load: [config],
+      envFilePath: loadEnvFilePath(),
+      load: [loadConfig],
       isGlobal: true,
     }),
     HealthModule,
